69de3d4199eb2154020524dd993b78b5eeb2b409
Maxim Vaculenco <maxim.vaculenco.jr@gmail.com>
2025-02-23 02:07:28 +0200

    compsport.sql

--- compsport.sql
+++ compsport.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jan 11, 2025 at 11:33 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
